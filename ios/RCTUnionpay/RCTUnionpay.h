3c7f255bb28c7086bf3d87bc9eeb10d3949a63f8
hanhuizhu <hanhuizhuxyl@gmail.com>
2019-11-05 17:45:19 +0800

    init

--- ios/RCTUnionpay/RCTUnionpay.h
+++ ios/RCTUnionpay/RCTUnionpay.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-#import "RCTBridgeModule.h"
+#import "React/RCTBridgeModule.h"
 #import "RCTLog.h"
 #import "RCTEventDispatcher.h"
 #import "RCTEventEmitter.h"
